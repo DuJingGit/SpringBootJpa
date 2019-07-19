886e0f93f2d297611adf4c8b9f187b870b988275
DuJingGit <3038852926@qq.com>
2019-07-19 10:57:55 +0800

    del

--- src/main/java/com/itmayiedu/dao/primary/UserDaoA.java
+++
@@ -1,9 +0,0 @@
-
-package com.itmayiedu.dao.primary;
-
-import com.itmayiedu.entity.primary.UserA;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDaoA extends JpaRepository<UserA, Integer> {
-
-}
--- src/main/java/com/itmayiedu/dao/second/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package com.itmayiedu.dao.second;
-
-import com.itmayiedu.entity.second.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDao extends JpaRepository<Role, Integer> {
-}
